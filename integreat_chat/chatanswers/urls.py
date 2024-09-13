0bc9b4d3d5c59b0ed1460ff8c35c461a832116c3
Sven Seeberg <mail@sven-seeberg.de>
2024-09-13 15:17:32 +0200

    Add Update VDB endpoint & return sources

--- integreat_chat/chatanswers/urls.py
+++ integreat_chat/chatanswers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("extract_answer/", views.extract_answer, name="extract_answer"),
+    path("update_vdb/", views.update_vdb, name="update_vdb")
 ]
--- integreat_chat/chatanswers/views.py
+++ integreat_chat/chatanswers/views.py
@@ -1,8 +1,18 @@
-from django.http import JsonResponse
-from .services.answer_service import AnswerService
-from django.views.decorators.csrf import csrf_exempt
+"""
+Django views
+"""
+
 import json
+import logging
+
+from integreat_chat.chatanswers.services.answer_service import AnswerService
+
+from .services.milvus import UpdateMilvus
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+
+LOGGER = logging.getLogger('django')
 
 @csrf_exempt
 def extract_answer(request):
@@ -10,11 +20,32 @@ def extract_answer(request):
     Extract an answer for a user query from Integreat content. Expects a JSON body with message
     and language attributes
     """
-    answer = None
+    result = None
     if request.method in ('POST') and request.META.get('CONTENT_TYPE').lower() == 'application/json':
         data = json.loads(request.body)
         question = data["message"]
         language = data["language"]
-        #answer_service = AnswerService.get_instance(language)
-        answer = settings.ANSWER_SERVICE.extract_answer(question)
-    return JsonResponse({"answer": answer})
+        region = data["region"]
+        LOGGER.error("Starting answer extraction.")
+        answer_service = AnswerService(region, language)
+        result = answer_service.extract_answer(question)
+        LOGGER.error("Finished answer extraction.")
+    return JsonResponse(result)
+
+@csrf_exempt
+def update_vdb(request):
+    """
+    Extract an answer for a user query from Integreat content. Expects a JSON body with message
+    and language attributes
+    """
+    if request.method in ('POST') and request.META.get('CONTENT_TYPE').lower() == 'application/json':
+        data = json.loads(request.body)
+        region = data["region"]
+        language = data["language"]
+        update_milvus = UpdateMilvus(region, language)
+        pages = update_milvus.fetch_pages_from_cms()
+        text_chunks = []
+        for page in pages:
+            text_chunks = text_chunks + update_milvus.split_page(page)
+        update_milvus.create_embeddings(text_chunks)
+    return JsonResponse({"status": "collection updated"})
