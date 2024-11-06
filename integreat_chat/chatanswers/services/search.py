daf30860e72e17a3cfea2e681d6b5336545af359
dasgoutam <goutam2475@gmail.com>
2024-11-06 12:10:24 +0100

    add entire page retrieval

--- integreat_chat/chatanswers/services/search.py
+++ integreat_chat/chatanswers/services/search.py
@@ -8,7 +8,9 @@ from pymilvus import (
 )
 
 from django.conf import settings
-
+import urllib.request
+import urllib.parse
+import json
 
 class SearchService:
     """
@@ -16,6 +18,7 @@ class SearchService:
     """
     def __init__(self, region, language):
         self.language = language
+        self.region = region
         self.vdb_host = settings.VDB_HOST
         self.vdb_port = settings.VDB_PORT
         self.vdb_collection_name = f"collection_ig_{region}_{language}"
@@ -38,6 +41,7 @@ class SearchService:
                     "source": source.entity.get('source'),
                     "score": source.distance
                 })
+        sources = sorted(sources, key=lambda x: x["score"])
         return sources
 
     def get_embeddings(self, question):
@@ -74,4 +78,39 @@ class SearchService:
             consistency_level="Strong",
             output_fields=(["source", "text"] if include_text else ["source"])
         )[0]
-        return self.doc_details(results, include_text)
+        sources = self.doc_details(results, include_text)
+        return self.retrieve_unique_pages(sources)
+
+    def retrieve_unique_pages(self, sources):
+        """
+        Get 3 unique pages from the sources retrieved from the retriever
+        """
+        unique_sources = []
+        top_pages = []
+        for source in sources:
+            if source['source'] not in unique_sources:
+                unique_sources.append(source['source'])
+            if len(unique_sources) == 3:
+                break
+        print(unique_sources)
+        for source in unique_sources:
+            top_pages.append(
+                    {
+                        "source": source,
+                        "text": self.fetch_page_from_cms(source)
+                    })
+        return top_pages
+
+    def fetch_page_from_cms(self, page_url):
+        """
+        get data from Integreat cms using the children endpoint
+        """
+        pages_url = f"https://cms-test.integreat-app.de/api/v3/{self.region}/{self.language}/children/?url={page_url}&depth=0"
+        encoded_url = urllib.parse.quote(pages_url, safe=':/=?&')
+        print(f"URL - {encoded_url}\n")
+        response = urllib.request.urlopen(encoded_url)
+        page = json.loads(response.read())[0]
+        if page["excerpt"]:
+            return page["excerpt"]
+        else:
+            return ""
