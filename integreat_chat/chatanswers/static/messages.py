87fc0d76a5ccee97f50f7c0ad73585b70f1f7c66
dasgoutam <goutam2475@gmail.com>
2025-01-24 14:28:50 +0100

    change message string

--- integreat_chat/chatanswers/static/messages.py
+++ integreat_chat/chatanswers/static/messages.py
@@ -6,4 +6,4 @@ class Messages:
 
     NO_ANSWER = """Sorry, we could not find an answer for you in the Integreat content. Please wait for a message from a human advisor."""
 
-    TALK_TO_HUMAN = """We will forward your request to a human counselor. Please wait."""
+    TALK_TO_HUMAN = """We will forward your request to a human advisor. Please wait."""
